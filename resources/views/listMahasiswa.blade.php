d46cb838504ec2df4f5538d83af7b686ef5506c4
ade-m <66313509+ade-m@users.noreply.github.com>
2022-03-25 20:29:16 +0700

    Tambah navabar pada setiap View

--- resources/views/listMahasiswa.blade.php
+++ resources/views/listMahasiswa.blade.php
@@ -1,24 +1,67 @@
-<html>
-    <head>
-        <title>List Profil Mahasiswa</title>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>List Profil Mahasiswa</title>
         <style>
             table, th, td {
             border: 1px solid black;
             border-collapse: collapse;
             }
         </style>
-    </head>
-    <body>
-        <table >
-            <tr >
-                <th>Nama</th>
-                <th>Jurusan</th>
-            </tr>
-           
-            @foreach ($mhs as $detailMhs)
-            <tr> <td>{{ $detailMhs['nama'] }} </td><td>  {{ $detailMhs['jurusan'] }}</td></tr>  
-            @endforeach
-           
-        </table>
-    </body>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">PT ABCD</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/Mahasiswa">List Mahasiswa</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/Mahasiswa/create">Tambah Mahasiswa</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <table >
+        <tr >
+            <th>Nama</th>
+            <th>Jurusan</th>
+        </tr>
+       
+        @foreach ($mhs as $detailMhs)
+        <tr> <td>{{ $detailMhs['nama'] }} </td><td>  {{ $detailMhs['jurusan'] }}</td></tr>  
+        @endforeach
+       
+    </table>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
 </html>
