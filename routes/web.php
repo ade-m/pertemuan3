3ae32d5af4cc84abf1d1aea1d36d82e5eaf1bcec
ade-m <66313509+ade-m@users.noreply.github.com>
2022-03-25 18:25:52 +0700

    Kirim Value melalui routes ke view

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome',['nama'=>'Budiono']);
 });
 
 
 Route::get('/Mahasiswa', function () {
-    return view('welcome');
+    return view('welcome',['nama'=>'Mahasiswa Budiono ']);
 });
