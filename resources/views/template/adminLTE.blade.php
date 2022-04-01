724302724b48954a6b06777fc4adde94bb73f138
ade-m <66313509+ade-m@users.noreply.github.com>
2022-04-01 19:40:15 +0700

    asset

--- resources/views/template/adminLTE.blade.php
+++ resources/views/template/adminLTE.blade.php
@@ -8,18 +8,18 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="adminLTE/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href='{{ asset("adminLTE/plugins/fontawesome-free/css/all.min.css") }}''>
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="adminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href='{{ asset("adminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css") }}'>
   <!-- Theme style -->
-  <link rel="stylesheet" href="adminLTE/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href='{{ asset("adminLTE/dist/css/adminlte.min.css") }}'>
 </head>
 <body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
 <div class="wrapper">
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__wobble" src="adminLTE/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__wobble" src='{{ asset("adminLTE/dist/img/AdminLTELogo.png") }}' alt="AdminLTELogo" height="60" width="60">
   </div>
 
   <!-- Navbar -->
@@ -71,7 +71,7 @@
           <a href="#" class="dropdown-item">
             <!-- Message Start -->
             <div class="media">
-              <img src="adminLTE/dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+              <img src='{{ asset("adminLTE/dist/img/user1-128x128.jpg") }}' alt="User Avatar" class="img-size-50 mr-3 img-circle">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
                   Brad Diesel
@@ -87,7 +87,7 @@
           <a href="#" class="dropdown-item">
             <!-- Message Start -->
             <div class="media">
-              <img src="adminLTE/dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <img src='{{ asset("adminLTE/dist/img/user8-128x128.jpg") }}' alt="User Avatar" class="img-size-50 img-circle mr-3">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
                   John Pierce
@@ -103,7 +103,7 @@
           <a href="#" class="dropdown-item">
             <!-- Message Start -->
             <div class="media">
-              <img src="adminLTE/dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <img src='{{ asset("adminLTE/dist/img/user3-128x128.jpg") }}' alt="User Avatar" class="img-size-50 img-circle mr-3">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
                   Nora Silvester
@@ -164,7 +164,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="adminLTE/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src='{{ asset("adminLTE/dist/img/AdminLTELogo.png") }}' alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
@@ -173,7 +173,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="adminLTE/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src='{{ asset("adminLTE/dist/img/user2-160x160.jpg") }}' class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
@@ -886,26 +886,26 @@
 
 <!-- REQUIRED SCRIPTS -->
 <!-- jQuery -->
-<script src="adminLTE/plugins/jquery/jquery.min.js"></script>
+<script src='{{ asset("adminLTE/plugins/jquery/jquery.min.js") }}'></script>
 <!-- Bootstrap -->
-<script src="adminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src='{{ asset("adminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js") }}'></script>
 <!-- overlayScrollbars -->
-<script src="adminLTE/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src='{{ asset("adminLTE/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js") }}'></script>
 <!-- AdminLTE App -->
-<script src="adminLTE/dist/js/adminlte.js"></script>
+<script src='{{ asset("adminLTE/dist/js/adminlte.js") }}'></script>
 
 <!-- PAGE PLUGINS -->
 <!-- jQuery Mapael -->
-<script src="adminLTE/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
-<script src="adminLTE/plugins/raphael/raphael.min.js"></script>
-<script src="adminLTE/plugins/jquery-mapael/jquery.mapael.min.js"></script>
-<script src="adminLTE/plugins/jquery-mapael/maps/usa_states.min.js"></script>
+<script src='{{ asset("adminLTE/plugins/jquery-mousewheel/jquery.mousewheel.js") }}'></script>
+<script src='{{ asset("adminLTE/plugins/raphael/raphael.min.js") }}'></script>
+<script src='{{ asset("adminLTE/plugins/jquery-mapael/jquery.mapael.min.js") }}'></script>
+<script src='{{ asset("adminLTE/plugins/jquery-mapael/maps/usa_states.min.js") }}'></script>
 <!-- ChartJS -->
-<script src="adminLTE/plugins/chart.js/Chart.min.js"></script>
+<script src='{{ asset("adminLTE/plugins/chart.js/Chart.min.js") }}'></script>
 
 <!-- AdminLTE for demo purposes -->
-<script src="adminLTE/dist/js/demo.js"></script>
+<script src='{{ asset("adminLTE/dist/js/demo.js") }}'></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="adminLTE/dist/js/pages/dashboard2.js"></script>
+<script src='{{ asset("adminLTE/dist/js/pages/dashboard2.js") }}'></script>
 </body>
 </html>
